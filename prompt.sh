51ebbddabd9c617fbf659ff1989b8a620b959fa8
adrishg <adri315289837@gmail.com>
2019-03-01 09:17:56 -0600

    Ya quedo (:

--- prompt.sh
+++ prompt.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
-trap '' 2 20 #Se encierra" los codigos de salida de Ctrl+Z (2) y Ctrl +C (20)
-trap '' SIGTSTP
+
 GREEN="\033[0;32m"
 NOCOLOR="\033[0m"
 
-#let valor=1 #Descomentar el valor si se quiere ingresar sin que sea de login
-
-if [ "$valor"=1 ]
+let valor=1
+if [ "$valor" = 1 ]
 then
 	clear
-	commandirri="ZZZ"
+	declare commandirri="hola"
 	
-	while [[ $commandirri != "salir" ]]
+	while [[ $commandirri != "exit" ]]
 	do
 		printf "${GREEN}$u @prebeshell: ${NOCOLOR}"
-		#read commandirri
-		IFS="" read -r commandirri parametro1 parametro2 > /dev/tty
-		if ! type "$comandirri" &> /dev/null; then
-			"$commandirri"
-		if [ "$commandirri" != "ZZZ" ] #|| ! type foobar &> /dev/null ]
+		read commandirri
+		if [ "$commandirri" != "hola" ]
 		then
-			if [ -f "${commandirri}".sh ]
+			if [ -f "$commandirri".sh ]
 			then
-				export parametro1
-				export parametro2
 				./"$commandirri".sh
 			else
 				echo "Ese comando no existe :c"
-				if [ "$commandirri" = "salir" ]
-				then
-					echo "Bueno, vai"
-				fi
+			fi
+			if [ "$commandirri" = "exit" ]
+			then
+				echo "Bueno, vai"
 			fi
 		fi
-	fi
 	done
 else
 		echo "Usted no ha ingresado como usuario, ciao"
