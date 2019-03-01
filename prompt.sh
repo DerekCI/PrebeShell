8dc460284fbf621ce320d31b1df07ff53f40df2d
fjdurlop <47513411+fjdurlop@users.noreply.github.com>
2019-03-01 12:23:23 -0600

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -1,39 +1,88 @@
 #!/bin/bash
 trap '' 2 20 #Se encierra" los codigos de salida de Ctrl+Z (2) y Ctrl +C (20)
-trap '' SIGTSTP
+trap "" SIGINT SIGTSTP
+ #Se encierra" los codigos de salida de Ctrl+Z (2) y Ctrl +C (20)
+trap "echo -n -e '   no'" INT
+
+
+ 	 
 GREEN="\033[0;32m"
 NOCOLOR="\033[0m"
 
-#let valor=1 #Descomentar el valor si se quiere ingresar sin que sea de login
-
-if [ "$valor"=1 ]
+let valor=1
+if [ "$valor" = 1 ]
 then
 	clear
-	commandirri="ZZZ"
+	declare commandirri="hola"
 	
 	while [[ $commandirri != "salir" ]]
 	do
 		printf "${GREEN}$u @prebeshell: ${NOCOLOR}"
-		#read commandirri
-		IFS=" " read -r commandirri parametro1 parametro2 > /dev/tty
-		if ! type "$comandirri" &> /dev/null; then
-			"$commandirri" &> /dev/null
-		fi
-		if [ "$commandirri" != "ZZZ" ] #|| ! type foobar &> /dev/null ]
-		then
-			if [ -f "${commandirri}.sh" ]
+		read commandirri parametro1 parametro2
+		echo $parametro1
+		echo $parametro2
+		if [ -f "$commandirri".sh ]
 			then
-				export parametro1
-				export parametro2
-				./"$commandirri".sh
-			else
-				if [ "$commandirri" = "salir" ]
-				then
-					echo "Bueno, vai"
+				if [ "$parametro1" != "" ]
+					then
+						if [ "$parametro2" != "" ]
+						then
+							hacer="./$commandirri.sh $parametro1 $parametro2"
+							$hacer
+						else
+							hacer="./$commandirri.sh $parametro1"
+							$hacer
+						fi
+						
+				else
+					./"$commandirri.sh"
 				fi
-				echo "Ese comando no existe :c"
-			fi
+				
+			else
+				if ! type foobar &> /dev/null; then
+					$commandirri
+				elif [ "$commandirri" != "hola" ]
+					then
+					if [ -f "$commandirri".sh ]
+					then
+						./"$commandirri".sh
+					else
+						echo "Ese comando no existe :c"
+					fi
+					if [ "$commandirri" = "exit" ]
+					then
+						echo "Bueno, vai"
+					fi
+				fi
 		fi
+		#if ! type foobar &> /dev/null; then
+		#	$commandirri
+		#elif [ "$commandirri" != "hola" ]
+		#	then
+		#	if [ -f "$commandirri".sh ]
+		#	then
+		#		./"$commandirri".sh
+		#	else
+		#		echo "Ese comando no existe :c"
+		#	fi
+		#	if [ "$commandirri" = "exit" ]
+		#	then
+		#		echo "Bueno, vai"
+		#	fi
+		#fi
+		#if [ "$commandirri" != "hola" ]
+		#then
+		#	if [ -f "$commandirri".sh ]
+		#	then
+		#		./"$commandirri".sh
+		#	else
+		#		echo "Ese comando no existe :c"
+		#	fi
+		#	if [ "$commandirri" = "exit" ]
+		#	then
+		#		echo "Bueno, vai"
+		#	fi
+		#fi
 	done
 else
 		echo "Usted no ha ingresado como usuario, ciao"
