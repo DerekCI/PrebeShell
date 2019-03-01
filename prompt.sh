0b6e2a4db6e1b6145a6685a8becc96e4cffb15b2
adrishg <adri315289837@gmail.com>
2019-03-01 11:41:52 -0600

    Ya, problema de versiones, perdón

--- prompt.sh
+++ prompt.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
-
-
-
+trap '' 2 20 #Se encierra" los codigos de salida de Ctrl+Z (2) y Ctrl +C (20)
+trap '' SIGTSTP
 GREEN="\033[0;32m"
 NOCOLOR="\033[0m"
 
-let valor=1
-if [ "$valor" = 1 ]
+#let valor=1 #Descomentar el valor si se quiere ingresar sin que sea de login
+
+if [ "$valor"=1 ]
 then
 	clear
-	declare commandirri="hola"
+	commandirri="ZZZ"
 	
-	while [[ $commandirri != "exit" ]]
+	while [[ $commandirri != "salir" ]]
 	do
 		printf "${GREEN}$u @prebeshell: ${NOCOLOR}"
-		IFS=" " read commandirri parametro1 parametro2
-		if [ "$commandirri" != "hola" ]
+		#read commandirri
+		IFS=" " read -r commandirri parametro1 parametro2 > /dev/tty
+		if ! type "$comandirri" &> /dev/null; then
+			"$commandirri" &> /dev/null
+		fi
+		if [ "$commandirri" != "ZZZ" ] #|| ! type foobar &> /dev/null ]
 		then
-			if [ -f "$commandirri".sh ]
+			if [ -f "${commandirri}.sh" ]
 			then
 				export parametro1
 				export parametro2
 				./"$commandirri".sh
 			else
+				if [ "$commandirri" = "salir" ]
+				then
+					echo "Bueno, vai"
+				fi
 				echo "Ese comando no existe :c"
-			fi
-			if [ "$commandirri" = "exit" ]
-			then
-				echo "Bueno, vai"
 			fi
 		fi
 	done
