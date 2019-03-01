bb32213a810b54bc3eaaa7d68f9e7d72c1353b74
adrishg <adri315289837@gmail.com>
2019-03-01 08:44:16 -0600

    Hi

--- prompt.sh
+++ prompt.sh
@@ -9,19 +9,21 @@ NOCOLOR="\033[0m"
 if [ "$valor"=1 ]
 then
 	clear
-	declare commandirri="ZZZ"
+	commandirri="ZZZ"
 	
 	while [[ $commandirri != "salir" ]]
 	do
 		printf "${GREEN}$u @prebeshell: ${NOCOLOR}"
 		#read commandirri
-		IFS="" read -r commandirri > /dev/tty
-		if ! type " " &> /dev/null; then
+		IFS="" read -r commandirri parametro1 parametro2 > /dev/tty
+		if ! type "$comandirri" &> /dev/null; then
 			"$commandirri"
-		elif [ "$commandirri" != "ZZZ" ] #|| ! type foobar &> /dev/null ]
+		if [ "$commandirri" != "ZZZ" ] #|| ! type foobar &> /dev/null ]
 		then
-			if [ -f "${commandirri}.sh" ]
+			if [ -f "${commandirri}".sh ]
 			then
+				export parametro1
+				export parametro2
 				./"$commandirri".sh
 			else
 				echo "Ese comando no existe :c"
@@ -31,8 +33,8 @@ then
 				fi
 			fi
 		fi
+	fi
 	done
 else
 		echo "Usted no ha ingresado como usuario, ciao"
 fi
-
